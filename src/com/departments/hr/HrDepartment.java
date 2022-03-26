d3897291a46598dfdbbbb86f9a1127d842dfe607
Gurpreet-Portfolio2022 <100070588+Gurpreet-Portfolio2022@users.noreply.github.com>
2022-03-26 16:30:05 +0530

    GurpreetSinghArora-OOPsAssignmentSolution-Rep3
    Gurpreet Singh Arora - OOPS Assignment Solution Rep3

---
+++ src/com/departments/hr/HrDepartment.java
@@ -0,0 +1,27 @@
+package com.departments.hr;
+
+import com.departments.superdepartment.SuperDepartment;
+
+public class HrDepartment extends SuperDepartment {
+	  
+	  
+	  public String departmentName()
+	  {
+	    return "HR Department";
+	  }
+	  
+	  public String getTodaysWork()
+	  {
+	    return "Fill today's worksheet and mark your attendance";            
+	  }
+	  public String  getWorkDeadline()
+	  {
+	    return "Complete by EOD ";          
+	  }
+	  public String doActivity()
+	  {
+	    return  "team Lunch";
+	  }
+	  
+
+	}
---
+++ src/com/main/Main.java
@@ -0,0 +1,41 @@
+package com.main;
+
+import com.departments.*;
+import com.departments.admin.AdminDepartment;
+import com.departments.hr.HrDepartment;
+import com.departments.tech.TechDepartment;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HrDepartment hrDepartment = new HrDepartment();
+		TechDepartment techDepartment = new TechDepartment();
+		AdminDepartment adminDepartment = new AdminDepartment();
+		
+	
+		System.out.println("Welcome to "+adminDepartment.departmentName());
+		System.out.println(adminDepartment.getTodaysWork());
+		System.out.println(adminDepartment.getWorkDeadline());
+		System.out.println(adminDepartment.isTodayAHoliday()  +"\n");
+		
+
+		System.out.println("Welcome to "+hrDepartment.departmentName());
+		System.out.println(hrDepartment.doActivity());
+		System.out.println(hrDepartment.getTodaysWork());
+		System.out.println(hrDepartment.getWorkDeadline());
+		System.out.println(hrDepartment.isTodayAHoliday()+"\n");
+		
+		
+		
+		System.out.println("Welcome to "+techDepartment.departmentName());
+		System.out.println(techDepartment.getTodaysWork());
+		System.out.println(techDepartment.getWorkDeadline());
+		System.out.println(techDepartment.getTechStackInformation());
+
+		System.out.println(techDepartment.isTodayAHoliday());
+		
+		
+		
+	}
+
+}
